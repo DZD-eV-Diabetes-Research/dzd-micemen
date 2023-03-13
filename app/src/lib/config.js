4df5028070f3e7d2d2cbafcef7b09effd2da4d2a
Philipp Schächtele <philipp@digitalwerft.com>
2023-03-13 11:44:13 +0100

    Major Update: Add fuctionality for Melodi Presto, GWAS, call app with ?gene= Param,  UI updates

--- app/src/lib/config.js
+++ app/src/lib/config.js
@@ -3,7 +3,8 @@
 const config = {
   apiBaseUrl: 'https://restapi.connect.dzd-ev.de/mouseclinic/',
   // used to build organim-specific table columns and API endpoints
-  organisms: ['human', 'mouse', 'zebrafish', 'rat', 'pig', 'c_elegans', 'fruitFly']
+  organisms: ['human', 'mouse', 'zebrafish', 'rat', 'pig', 'c_elegans', 'fruitFly'],
+  defaultLimit: 20
 };
 
 export default config;
