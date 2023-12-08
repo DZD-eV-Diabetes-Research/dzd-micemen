dc7b04f235b5320241e264768420fe0fc266f3d5
motey <bleimehl@gmail.com>
2023-12-08 10:03:12 +0100

    adapt default path

--- app/src/lib/config.js
+++ app/src/lib/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   //apiBaseUrl: 'https://restapi.connect.dzd-ev.de/mouseclinic/',
-  apiBaseUrl: window.Env['API_SERVER_BASE_URL'] || '/',
+  apiBaseUrl: window.Env['API_SERVER_BASE_URL'] || '/mouseclinic/',
   // apiBasePath: 'https://api-micemen.dzd-ev.org/',
   apiBasePath: window.Env['API_SERVER_BASE_PATH'] || '/',
   // used to build organim-specific table columns and API endpoints
