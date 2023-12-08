28d0c74a60dd18cbc260e8ed23490186a2005012
motey <bleimehl@gmail.com>
2023-12-08 09:58:34 +0100

    fix merge conflict

--- app/src/lib/config.js
+++ app/src/lib/config.js
@@ -2,14 +2,9 @@
 
 const config = {
   //apiBaseUrl: 'https://restapi.connect.dzd-ev.de/mouseclinic/',
-<<<<<<< HEAD
-  apiBaseUrl: 'https://api-micemen.dzd-ev.org/',
-  apiBasePath: 'https://api-micemen.dzd-ev.org/mouseclinic/',
-=======
   apiBaseUrl: window.Env['API_SERVER_BASE_URL'] || '/',
   // apiBasePath: 'https://api-micemen.dzd-ev.org/',
   apiBasePath: window.Env['API_SERVER_BASE_PATH'] || '/',
->>>>>>> development
   // used to build organim-specific table columns and API endpoints
   organisms: ['human', 'mouse', 'zebrafish', 'rat', 'pig', 'c_elegans', 'fruitFly'],
   defaultLimit: 20
